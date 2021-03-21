87088e5d7cc27c24003b11391be4ee2d63a0951a
zenoleg <zenkovicholeg@gmail.com>
2021-03-21 16:19:49 +0300

    1.10 ex

---
+++ src/1.10/main.go
@@ -0,0 +1,60 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 17.
+//!+
+
+// Fetchall fetches URLs in parallel and reports their times and sizes.
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	ch := make(chan string)
+	for _, url := range os.Args[1:] {
+		go fetch(url, ch) // start a goroutine
+	}
+	for range os.Args[1:] {
+		fmt.Println(<-ch) // receive from channel ch
+	}
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+}
+
+func fetch(url string, ch chan<- string) {
+	start := time.Now()
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprint(err) // send to channel ch
+		return
+	}
+
+	fileName := strings.Replace(url, "https://", "", 1)
+	file, _ := os.Create(fileName)
+
+	nbytes, err := io.Copy(file, resp.Body)
+
+	fileErr := file.Close()
+
+	if fileErr != nil {
+		ch <- fmt.Sprintf("File error %s: %v", url, err)
+		return
+	}
+
+	resp.Body.Close() // don't leak resources
+	if err != nil {
+		ch <- fmt.Sprintf("while reading %s: %v", url, err)
+		return
+	}
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2fs  %7d  %s", secs, nbytes, url)
+}
+
+//!-
